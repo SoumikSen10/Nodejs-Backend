0a4d7e0cb8aeff146e0676a8669531cd852dc08b
Soumik Sen <mastersoumik2003@gmail.com>
2024-05-02 01:34:35 +0530

    Minor update

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,20 +6,3 @@ const asyncHandler = (requestHandler) => {
 };
 
 export { asyncHandler };
-
-//Using async await
-/* const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
- */
-/*Explanation
-const asyncHandler=()=>{}
-const asyncHandler=(func)=>()=>{}
-const asyncHandler=(func)=>async()=>{} */
